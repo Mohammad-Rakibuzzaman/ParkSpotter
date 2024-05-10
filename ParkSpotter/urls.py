a5a8aa410f5aee1797f8897cd881ef88a4e91e7e
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2024-05-10 14:30:26 +0600

    home page added on project folder

--- ParkSpotter/urls.py
+++ ParkSpotter/urls.py
@@ -2,9 +2,11 @@
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
