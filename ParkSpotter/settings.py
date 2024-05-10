8b61670ecc9397900661ad39257c6e6eabbc203a
atiarrahaman <mowuj92@gmail.com>
2024-05-10 20:36:51 +0600

    [Modify] set public  email and password for test

--- ParkSpotter/settings.py
+++ ParkSpotter/settings.py
@@ -149,5 +149,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = env("EMAIL")
-EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD")
+EMAIL_HOST_USER = 'mowuj1320@gmail.com'
+EMAIL_HOST_PASSWORD = 'tuyc emix hphd hgbu'
