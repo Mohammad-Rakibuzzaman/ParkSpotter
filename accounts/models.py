8bf48cba08473653b1bc9c6561f0c57ac89f6c93
atiarrahaman <mowuj92@gmail.com>
2024-05-12 20:41:10 +0600

    [Add] Add Employee, Vehicle,Booking

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 import math
+from .constants import TIME_SLOT, PARK_PLAN
 
 # Create your models here.
 
@@ -23,31 +24,74 @@ class ParkOwner(models.Model):
     def __str__(self):
         return self.park_owner_id.username
 
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    mobile_no = models.CharField(max_length=12)
+    qualification = models.CharField(max_length=50, null=True, blank=True)
+    nid_card_no = models.CharField(max_length=11)
+    address = models.CharField(max_length=200, blank=True, null=True)
+    joined_date = models.DateTimeField(
+        auto_now_add=True, null=True, blank=True)
+
 
 class Park_Detail(models.Model):
     park_owner = models.ForeignKey(
         ParkOwner, related_name="park_details", on_delete=models.CASCADE)
-    capacity = models.PositiveIntegerField(default=5)
-    park_plan_text = models.TextField(blank=True, null=True)
+    capacity = models.PositiveIntegerField(default=10)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def str(self):
+        return f"Park Detail for {self.park_owner.id}"
+
+    # def save(self, *args, **kwargs):
+    #     # Generate the park_plan_text before saving
+    #     self.park_plan_text = self.generate_park_plan_text()
+    #     super(Park_Detail, self).save(*args, **kwargs)
+
+    # def generate_park_plan_text(self):
+    #     park_plan_text = ""
+
+    #     for i in range(1, self.capacity + 1):
+    #         park_plan_text += f"(1,{i}) "
+
+    #     return park_plan_text.strip()
+
+
+class Vehicle(models.Model):
+    plate_number=models.CharField(max_length=20)
+    mobile_no = models.CharField(max_length=12)
+
+PARK_PLAN=[
+    ('1','1'),
+    ('2','2'),
+    ('3','3'),
+    ('4','4'),
+    ('5','5'),
+    ('6','6'),
+    ('7','7'),
+    ('8','8'),
+    ('9','9'),
+    ('10','10')
+]
+class Booking (models.Model):
+    park_detail=models.ForeignKey(Park_Detail, on_delete=models.CASCADE)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    slot = models.IntegerField(choices=PARK_PLAN,default=1)
+    fine=models.IntegerField(default=0,null=True,blank=True)
+    time_slot = models.IntegerField(choices=TIME_SLOT)
+    check_in_time = models.DateTimeField(auto_now_add=True)
+    check_out_time = models.DateTimeField(blank=True, null=True)
+
+    @property
+    def amount(self):
+        if self.time_slot == '1':
+            return 30
+        elif self.time_slot == '2':
+            return 50
+        elif self.time_slot == '3':
+            return 100
+        else:
+            return 0 
+
     def __str__(self):
-        return f"Park Detail for {self.park_owner}"
-
-    def generate_park_plan_text(self):
-        rows = int(math.ceil(self.capacity / 5))
-        cols = 5
-
-        park_plan_text = ""
-
-        spot_count = 1
-        for i in range(rows):
-            for j in range(cols):
-                if spot_count <= self.capacity:
-                    park_plan_text += f"({i+1},{j+1}) "
-                    spot_count += 1
-                else:
-                    break
-            park_plan_text += "\n"
-
-        return park_plan_text
+        return f"Booking for {self.vehicle} ({self.get_time_slot_display()})"
